fe7564025144bf6e7a292bb59fd34b3c9120b243
Tichin Wu <twu38@mail.ccsf.edu>
2025-09-28 09:13:36 -0700

    git commit -m "feat: add UserEditForm for profile editing"

--- forms.py
+++ forms.py
@@ -46,5 +46,40 @@ class LoginForm(FlaskForm):
         validators=[InputRequired(), Length(min=6, max=50)],
     )
 
+
 class CSRFProtectionForm(FlaskForm):
     """CSRF form."""
+
+
+class UserEditForm(FlaskForm):
+    """Form for editing users."""
+
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    email = StringField(
+        'E-mail',
+        validators=[InputRequired(), Email(), Length(max=50)],
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
+    )
+
+    image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    header_image_url = StringField(
+        '(Optional) Header Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    bio = TextAreaField(
+        '(Optional) Tell us about yourself',
+        validators=[Optional()]
+    )
